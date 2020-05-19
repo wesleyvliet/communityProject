425ab65b761e2c7f5a29d4de41af51955def030b
wesleyvliet <507503@edu.rocmn.nl>
2020-05-19 12:53:26 +0200

    test2

---
+++ controller/ContactsController.php
@@ -0,0 +1,131 @@
+<?php
+require_once 'model/ContactsLogic.php';
+class ContactsController{
+
+	public function __construct() {
+		$this->ContactsLogic = new ContactsLogic();
+	}
+
+	public function handleRequest()
+	{
+		try {
+			$op = isset($_REQUEST['op'])?$_REQUEST['op']:NULL;
+			$url = strtolower($_GET['url']);
+			if(!empty($op)) {
+				switch ($op) {
+					case 'login':
+					$this->collectReadAdmin($_REQUEST['userName'], $_REQUEST['userPass']);
+					break;
+					case 'create':
+					$this->collectCreateCompetition($_REQUEST['contestTitle'], $_REQUEST['contestGame'], $_REQUEST['contestDescription'], $_REQUEST['contestTime'], $_REQUEST['contestDate'], $_REQUEST['contestCompetitorsA'], $_REQUEST['contestCompetitorsB']);
+					break;
+					case 'delete-game':
+					$this->collectDeleteGame($_REQUEST['id']);
+					break;
+					case 'undo-delete':
+					$this->collectUndoDelete($_REQUEST['id']);
+					break;
+					default:
+					echo 'sorry kan deze pagina: ' . $op . ' niet vinden :(';
+					break;
+				}
+			} else {
+				switch ($url) {
+					case 'admin':
+						include 'view/login.php';
+						break;
+					case 'dashboard':
+						include 'view/dashboard.php';
+						break;
+					case 'nieuwe-wedstrijden':
+						include 'view/addContest.php';
+						break;
+					case 'nieuwe-wedstrijden-competitors':
+						$this->collectReadCompetitors($_REQUEST['contestTitle'], $_REQUEST['contestGame'], $_REQUEST['contestDescription'], $_REQUEST['contestAmount'], $_REQUEST['contestTime'], $_REQUEST['contestDate']);
+						break;
+					case 'overview-wedstrijden':
+						$this->collectAllGames();
+						break;
+					default:
+						echo 'sorry kan deze pagina: ' . $url . ' niet vinden :(';
+						break;
+				}
+			}
+		} catch (ValidationException $e) {
+				$errors = $e->getErrors();
+		}
+
+	}
+
+	public function collectCreateCompetition($title, $game, $description, $time, $date, $contestCompetitorsA, $contestCompetitorsB) {
+		$create = $this->ContactsLogic->createCompetition($title, $game, $description, $time, $date, $contestCompetitorsA, $contestCompetitorsB);
+		$id = intval($create);
+		if($id >= 1) {
+			$message = 'wedstrijd is aangemaakt';
+			include 'view/addContest.php';
+		} else {
+			$message = 'wedstrijd is niet aangemaakt proebeer het opnieuw';
+			include 'view/addContest.php';
+		}
+	}
+
+	public function collectReadCompetitors($title, $game, $description, $competitorsAmount, $time, $date){
+		$check = $this->ContactsLogic->checkDataContest($title, $game, $description, $competitorsAmount, $time, $date);
+		if($check == false) {
+			$competitors = $this->ContactsLogic->fetchCompetitors();
+		} else {
+			$error = 'Ongeldige gegevens bij het inlogen controleer de velden opnieuw';
+		}
+		include 'view/addContest.php';
+	}
+
+	public function collectReadAdmin($userName, $userPass){
+		$check = $this->ContactsLogic->checkData($userName, $userPass);
+		if($check == false) {
+			$admin = $this->ContactsLogic->readAdmin($userName, $userPass);
+			if(empty($admin)) {
+				$error = "Verkeerde wachtwoord of gebruikersnaam";
+				include 'view/login.php';
+			} else {
+				session_name("admin");
+				session_start();
+				$_SESSION["name"] = $admin['username'];
+				$_SESSION["sesionId"] = "83523489765735412414";
+				header('Location: dashboard');
+			}
+		} else {
+			$error = 'Ongeldige gegevens bij het inlogen controleer de velden opnieuw';
+			include 'view/login.php';
+		}
+		//include 'view/reads.php';
+	}
+
+	public function collectReadContact($id){
+
+	}
+
+	public function collectUpdateContact(){
+
+	}
+
+	public function collectDeleteGame($id){
+		$message = $this->ContactsLogic->archiveGame($id);
+		$this->collectAllGames($message);
+	}
+
+	public function collectUndoDelete($id) {
+		$message = $this->ContactsLogic->undoDelete($id);
+		$this->collectAllGames($message);
+	}
+
+	public function __destruct(){
+
+	}
+
+	public function collectAllGames($message = null) {
+		$overview = $this->ContactsLogic->fetchAllGames($message);
+		include 'view/overviewGames.php';
+	}
+}
+
+?>
