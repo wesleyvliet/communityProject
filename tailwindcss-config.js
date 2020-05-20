74dd211cf33eb0e6a85032f359221973a27f8974
Remco <rvoudheusden@hotmail.nl>
2020-05-20 03:25:44 +0200

    article page added

---
+++ tailwindcss-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+      fontFamily: {
+          sans: ['Lato', 'sans-serif'],
+          display: ['Lato', 'sans-serif']
+      },
+      extend: {}
+  },
+  variants: {},
+  plugins: []
+};
