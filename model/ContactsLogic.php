6f21505eb13e24837d806b3ddd026fe5938bdd33
AnonymousSmurf <45464235+AnonymousSmurf@users.noreply.github.com>
2020-05-27 03:52:12 +0200

    Fixed Teams :D

--- model/ContactsLogic.php
+++ model/ContactsLogic.php
@@ -595,7 +595,7 @@ class ContactsLogic {
 		if (!isset($imageFile) || $imageFile['size'] == 0) {
 			$image = $image;
 		} else {
-			$image = $imageFile;
+			$image = $imageFile["name"];
 		}
 		$sql = "UPDATE articles SET title='$title', content='$text', categorie='$categorie', author='$author', preview_image='$image' WHERE id=$id";
 		$this->DataHandler->updateData($sql);
@@ -701,8 +701,6 @@ class ContactsLogic {
 				<tr>
 					<th class="p-3 font-bold uppercase hidden lg:table-cell">Naam</th>
 					<th class="p-3 font-bold uppercase hidden lg:table-cell">Logo</th>
-					<th class="p-3 font-bold uppercase hidden lg:table-cell">Aanpassen</th>
-					<th class="p-3 font-bold uppercase hidden lg:table-cell">Verwijderen</th>
 				</tr>
 			</thead>
 			<tbody>';
@@ -718,33 +716,20 @@ class ContactsLogic {
 					<img class="w-25 " src="view/assets/img/'.$row['logo'].'"/>
 				</div>
 			</td>
-			<td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-			<span class="lg:hidden absolute text-white bg-gray-800 rounded-r-lg inset-y-0 left-0 py-4 text-xs font-bold uppercase w-32">Aanpassen</span>
-			<a href="?op=edit-team&id=' . $row['id'] . '">
-				<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15"><path class="heroicon-ui" d="M9 4.58V4c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v.58a8 8 0 0 1 1.92 
-						1.11l.5-.29a2 2 0 0 1 2.74.73l1 1.74a2 2 0 0 1-.73 2.73l-.5.29a8.06 8.06 0 0 1 0 2.22l.5.3a2 2 0 0 1 .73 2.72l-1 1.74a2 2 0 0 1-2.73.73l-.5-.3A8 8 0 0 1 15 
-						19.43V20a2 2 0 0 1-2 2h-2a2 2 0 0 1-2-2v-.58a8 8 0 0 1-1.92-1.11l-.5.29a2 2 0 0 1-2.74-.73l-1-1.74a2 2 0 0 1 .73-2.73l.5-.29a8.06 8.06 0 0 1 0-2.22l-.5-.3a2 2 0 0 1-.73-2.72l1-1.74a2 
-						2 0 0 1 2.73-.73l.5.3A8 8 0 0 1 9 4.57zM7.88 7.64l-.54.51-1.77-1.02-1 1.74 1.76 1.01-.17.73a6.02 6.02 0 0 0 0 2.78l.17.73-1.76 1.01 1 1.74 1.77-1.02.54.51a6 6 0 0 0 2.4 
-						1.4l.72.2V20h2v-2.04l.71-.2a6 6 0 0 0 2.41-1.4l.54-.51 1.77 1.02 1-1.74-1.76-1.01.17-.73a6.02 6.02 0 0 0 0-2.78l-.17-.73 1.76-1.01-1-1.74-1.77 1.02-.54-.51a6 6 0 0 
-						0-2.4-1.4l-.72-.2V4h-2v2.04l-.71.2a6 6 0 0 0-2.41 1.4zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
-					</svg>
-				</button>
-			</a>
-		</td>
-		<td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
-			<span class="lg:hidden absolute text-white bg-gray-800 rounded-r-lg inset-y-0 left-0 py-2 text-xs font-bold uppercase w-32">Verwijderen</span>
-			<a href="?op=delete-team&id=' . $row['id'] . '">
-				<button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-					<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 
-						.552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/>
-					</svg>
-				</button>
-			</a>
-		</td>
+			
 	</tr>';
 	}
 	$teams .= '</tbody></table>'.$message;
 	return $teams;
+	}
+
+	public function createTeam($name, $logo) {
+		$this->uploadFile($logo);
+		$logo = $logo["name"];
+
+		$sql = "INSERT INTO competitors (id, name, logo) VALUES (NULL, '$name', '$logo')";
+		$this->DataHandler->createData($sql);
+		$message = "Team toegevoed";
+		return $message;
 	}
 }
---
+++ view/addTeam.php
@@ -0,0 +1,85 @@
+<?php require_once 'view/header.php'; ?>
+<body>
+
+    <!-- Nav menu CMS START-->
+    <nav class="flex items-center justify-between flex-wrap bg-indigo-800 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <span class="font-semibold text-xl tracking-tight">G69</span>
+        </div>
+        <div class="block lg:hidden">
+          <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+          </button>
+        </div>
+      </nav>
+    <!-- Nav menu CMS END -->
+
+    <!-- Terug knop START -->
+    <div class="flex justify-between">
+		<div class="flex md:px-20 py-8 w-full mx-auto px-4 sm:px-8">
+            <a href="overview-teams">
+                <button class="bg-transparent hover:bg-indigo-800 text-blue-700 font-semibold hover:text-white py-3 px-5 border border-blue-500 hover:border-transparent rounded">
+                    Terug
+                </button>
+            </a>
+        </div>
+    </div>
+    <!-- Terug knop END -->
+
+    <!-- Form START -->
+    <div class="flex justify-center items-center py-8">
+        <form action="?op=add-team" method="post" enctype='multipart/form-data' class="max-w-lg border border-gray-200 shadow-xs mx-auto rounded-lg p-10 bg-white text-center space-y-6 flex-grow">
+            <div class="flex flex-col">
+                <label for="titel" class="self-start mb-2 font-medium text-gray-800">
+                    Naam
+                </label>
+
+                <input type="text" id="name" name="name" placeholder="Naam organisatie" class="outline-none px-2 py-2 border shadow-sm placeholder-gray-500 opacity-50 rounded" required>
+            </div>
+
+            <div class="flex items-center text-gray-800">
+                <span class="block border border-gray-400 w-2/4 mr-2"></span>
+                    Afbeelding preview
+                <span class="block border border-gray-400 w-2/4 ml-2"></span>
+            </div>
+            <div class="flex justify-around text-center divide-x divide-gray-300 p-8">
+
+                <img class="object-none object-center outline-none px-20 py-20 border shadow-sm rounded" id="output"/>
+            </div>
+
+            <div class="flex items-center text-gray-800">
+                <span class="block border border-gray-400 w-1/2 mr-2"></span>
+                    Afbeelding Upload
+                <span class="block border border-gray-400 w-1/2 ml-2"></span>
+            </div>
+
+            <div class="flex justify-around items-center text-center divide-gray-300">
+                <div class="flex w-full items-center justify-center bg-grey-lighter">
+                    <label class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
+                        <span class="text-base leading-normal">Select a file</span>
+                        <input type='file' name="logo" accept="image/*" onchange="loadFile(event)" class="hidden" required />
+                        <script>
+                            var loadFile = function(event) {
+                              var output = document.getElementById('output');
+                              output.src = URL.createObjectURL(event.target.files[0]);
+                              output.onload = function() {
+                                URL.revokeObjectURL(output.src)
+                              }
+                            };
+                        </script>
+                    </label>
+                </div>
+            </div>
+
+            <div class="flex items-center text-gray-800 p-8">
+                <span class="w-1/2"></span>
+                    <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-10 rounded float-right">
+                        Team toevoegen
+                    </button>
+                <span class="w-1/2"></span>
+            </div>
+            
+        </form>
+    </div>
+
+<?php require_once 'view/footer.php'; ?>
