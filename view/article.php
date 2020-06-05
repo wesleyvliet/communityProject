ae0cc6caf8d0c4f1712858981f6f3c54c04b6e8e
Remco <rvoudheusden@hotmail.nl>
2020-06-05 16:03:33 +0200

    Front end comment section added

--- view/article.php
+++ view/article.php
@@ -70,7 +70,44 @@ require_once "view/header.php";
             <p>
                 <?php echo $article["content"]; ?>
             </p>
-            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button  " data-text="Look at this awsome article about G69" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button   " data-text="Look at this awsome article about G69" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <!-- comment section -->
+            <div class="  w-full  mt-10 flex-row mb-10 ">
+                <!-- Comment -->
+               
+               <div class="flex flex-col md:flex-row  mr-auto ml-auto mb-5  ">
+                <!-- Comment IMG -->
+                <div class="m-2 flex-col mr-auto ml-auto flex-none items-left md:text-center "><img src="./view/assets/img/icon2.jpg" class=" h-10 w-10 lg:h-20 lg:w-20 rounded-full text-center " alt="">jasper</div>
+                <!-- Comment text -->
+                <div class="  border-2 rounded-lg px-4 py-2 m-2 flex align-middle text-white text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, dolores quasi. Sequi ipsa soluta architecto neque voluptate quaerat inventore sapiente aut, incidunt ut blanditiis esse provident dolorum saepe accusamus quod?</div>                      
+            
+            </div>
+               <!-- Comment -->
+               
+               <div class="flex flex-col md:flex-row w-3/3 mr-auto ml-auto mb-5  ">
+                <!-- Comment IMG -->
+                <div class="     m-2 mr-auto ml-auto flex-col flex-none items-left md:text-center"><img src="./view/assets/img/icon2.jpg" class=" h-10 w-10 lg:h-20 lg:w-20 rounded-full text-center " alt="">jasper</div>
+                <!-- Comment text -->
+                <div class="  border-2 rounded-lg px-4 py-2 m-2 flex align-middle text-white text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, dolores quasi. Sequi ipsa soluta architecto neque voluptate quaerat inventore sapiente aut, incidunt ut blanditiis esse provident dolorum saepe accusamus quod?</div>                      
+            
+            </div>
+                <!-- Comment END -->
+
+                
+                    <div class="w-full mr-auto ml-auto mt-10  lg:w-2/3  mb-2 mt-2 ">
+                        <form action="">
+                        <input type="text" class="bg-gray-100 mb-5 text-black rounded border border-gray-400 leading-normal resize-none w-full h-10 py-2 px-3  placeholder-gray-700 focus:outline-none focus:bg-white " name="name" placeholder='Your name' required>
+                        <textarea class="bg-gray-100 mb-5 text-black rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="message" placeholder='Type Your Comment' required></textarea>
+                        
+                            <input type='submit' class="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg  mr-1 hover:bg-gray-400" value='Post Comment'>
+                        </form>
+                    </div>
+                </div>
+               
+           
+             <!-- Commment section END -->
+
+          
         </div>
         
     </div>
